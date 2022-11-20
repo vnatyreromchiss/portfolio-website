58dcae371d5df3ceca1d09fafb18eb0954804e5e
vnatyreromchiss <rdg2039@gmail.com>
2022-11-19 20:00:24 -0600

    master

--- send.php
+++ send.php
@@ -67,41 +67,41 @@
 
 
 
-    $name = $_POST['name'];
-    $project = $_POST['project'];
-    $email = $_POST['email'];
-    $message = $_POST['message'];
+    // $name = $_POST['name'];
+    // $project = $_POST['project'];
+    // $email = $_POST['email'];
+    // $message = $_POST['message'];
 
 
-    $name = htmlspecialchars($name);
-    $project = htmlspecialchars($project);
-    $email = htmlspecialchars($email);
-    $message = htmlspecialchars($message);
+    // $name = htmlspecialchars($name);
+    // $project = htmlspecialchars($project);
+    // $email = htmlspecialchars($email);
+    // $message = htmlspecialchars($message);
 
-    $name = urldecode($name);
-    $project = urldecode($project);
-    $email = urldecode($email);
-    $message = urldecode($message);
+    // $name = urldecode($name);
+    // $project = urldecode($project);
+    // $email = urldecode($email);
+    // $message = urldecode($message);
 
-    $name = trim($name);
-    $project = trim($project);
-    $email = trim($email);
-    $message = trim($message);
+    // $name = trim($name);
+    // $project = trim($project);
+    // $email = trim($email);
+    // $message = trim($message);
 
 
-    if(mail("rdg2039@gmail.com",
-            "Новое письмо с сайта портфолио",
-            "Имя: " .$name."\n".
-            "Проект: " .$project."\n".
-            "Email: " .$email."\n".
-            "Сообщение: " .$message,
-            "From: random@gmail.com \r\n")
-    ) {
-        echo ('Message sent successfully!');
-    }
+    // if(mail("rdg2039@gmail.com",
+    //         "Новое письмо с сайта портфолио",
+    //         "Имя: " .$name."\n".
+    //         "Проект: " .$project."\n".
+    //         "Email: " .$email."\n".
+    //         "Сообщение: " .$message,
+    //         "From: random@gmail.com \r\n")
+    // ) {
+    //     echo ('Message sent successfully!');
+    // }
 
-    else {
-        echo('Error! Check your information...');
-    }
+    // else {
+    //     echo('Error! Check your information...');
+    // }
 
 ?>
