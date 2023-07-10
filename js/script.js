5d2e7cc9210423deafb2f38c2ce9cddc2d4f776c
vnatyreromchiss <rdg2039@gmail.com>
2023-07-09 20:43:49 -0500

    new section courses and new projects in portfolio

--- js/script.js
+++ js/script.js
@@ -107,6 +107,20 @@ const swiper = new Swiper('.portfolio__container', {
     //   clickable: true,
     // },
 });
+/*==================== COURSES SWIPER  ====================*/
+const swiperCourses = new Swiper('.courses__container', {
+    cssMode: true,
+    loop:true,
+
+    // navigation: {
+    //   nextEl: '.swiper-button-next',
+    //   prevEl: '.swiper-button-prev',
+    // },
+    // pagination: {
+    //   el: '.swiper-pagination',
+    //   clickable: true,
+    // },
+});
 
 /*==================== CONTACT FORM ====================*/
 $(document).ready(function()
